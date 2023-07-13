b2ee5af26af16fa291aa3614a2608697e4c18801
kaleikautakaoka <sachikaleikautakaoka@gmail.com>
2023-07-13 00:21:35 -0600

    reshuffling of code

--- index.js
+++ index.js
@@ -5,21 +5,20 @@ const { Shape, Circle, Square, Triangle } = require('./lib/Shapes.js');
 const questions = require('./lib/question.js');
 
 class SVG {
-    constructor(Shape, Text_color, Background_color) {
-        this.letter = 'letter';
-        this.Shape = 'Shape';
-        this.Text_color = 'Color';
-        this.Background_color = 'Background';
+    constructor(textColor, backgroundColor, letter) {
+        this.letter = letter;
+        this.textColor = textColor;
+        this.backgroundColor = backgroundColor;
     }
     render() {
         return `<svg width="100" height="100" xmlns="http://>
-        <rect width="100" height="100" style="fill:${this.Background_color};stroke-width:3;stroke:rgb(0,0,0)" />
-        <text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" style="fill:${this.Text_color};">${this.letter}</text>
+        <rect width="100" height="100" style="fill:${this.backgroundColor};stroke-width:3;stroke:rgb(0,0,0)" />
+        <text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" style="fill:${this.textColor};">${this.letter}</text>
         </svg>`
     }
 }
 
-function writeToFile(fileName, data) {
+function writeToFile(shape, fileName) {
     fs.writeFile(fileName, logo.svg, function (err) {
         if (err) {
             return console.log(err);
@@ -27,17 +26,20 @@ function writeToFile(fileName, data) {
         console.log("Success!");
     });
     // Shape.setColor(response.Text_color);
-    Shape.render();
-    fs.writeFile("logo.svg", Shape.render());
+    shape.render();
+    fs.writeFile("logo.svg", shape.render());
 }
 
 let shape;
 let shapeChoices = [];
 
-function init(Shape, Circle, Square, Triangle) {
+function init() {
     inquirer.prompt(questions).then((response) => {
         console.log(response);
-        if (response.shape == 'Circle') {
+        
+        //if property: shape on the object: response, which is [ "string"] is equals to the string literal "circle"
+            //then do some work
+        if (response.shape.any(a => a === 'Circle')) {
             shape = new Circle(response.letter, response.Text_color, response.Background_color);
             shapeChoices = response.shape;
         } else if (response.shape == 'Square') {
