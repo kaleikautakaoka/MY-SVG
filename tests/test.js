022dfaf7473f57ddf8be7e0f0cbe50bfdcd07c91
kaleikautakaoka <sachikaleikautakaoka@gmail.com>
2023-07-16 23:17:47 -0600

    test.js updated

--- tests/test.js
+++ tests/test.js
@@ -1,33 +1,27 @@
 const { circle, square, triangle } = require('../lib/Shapes.js');
 
-describe('triangle', () => {
-    test('creates a triangle object', () => {
-        const myNewColor = 'red';
-        const expectations = `<polygon points="150, 18 244, 182 56, 182" fill="red" />`;
-        const newShape = new triangle([myNewColor]);
-        expect(newShape.render()).toEqual(expectations);
-    })
-});
-
-describe('circle', () => {
-    test('creates a circle object', () => {
-        const myNewColor = 'red';
-        const expectations = `<circle cx="150" cy="150" r="80" fill="red" />`;
-        const newShape = new circle([myNewColor]);
-        expect(newShape.render()).toEqual(expectations);
-    })
+test('circle constructor', () => {
+    const c = new circle('A', 'red');
+    expect(c.letter).toBe('A');
+    expect(c.backgroundColor).toBe('red');
 }
 );
 
-describe('square', () => {
-    test('creates a square object', () => {
-        const myNewColor = 'red';
-        const expectations = `<rect x="50" y="50" width="200" height="200" fill="red" />`;
-        const newShape = new square([myNewColor]);
-        expect(newShape.render()).toEqual(expectations);
-    })
+test('square constructor', () => {
+    const s = new square('B', 'blue');
+    expect(s.letter).toBe('B');
+    expect(s.backgroundColor).toBe('blue');
 }
 );
+
+test('triangle constructor', () => {
+    const t = new triangle('C', 'green');
+    expect(t.letter).toBe('C');
+    expect(t.backgroundColor).toBe('green');
+}
+);
+
+
 
 
 
